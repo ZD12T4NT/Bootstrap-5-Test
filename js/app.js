2521f65ece5524544c7cc0db5d6d3269e02b5991
Zac Distant <zacdistant@gmail.com>
2022-05-20 06:40:07 +0100

    Bootstrap5 testing

---
+++ js/app.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiemFjZGlzdGFudCIsImEiOiJjbDNkNDF1NG8wMXhxM2RxbDhidmt3ajViIn0.f0ju41AIf8MpAtbiQAuGvQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center:[-71.06982, 42.35725],
+zoom: 18
+});
